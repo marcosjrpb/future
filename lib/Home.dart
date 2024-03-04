f52df4956bf2f054bf24055b92ab4a47378bfc08
marcosjrpb <marcosjrpb@hotmail.com>
2024-03-04 13:29:46 -0300

    segunda parte do Future

--- lib/Home.dart
+++ lib/Home.dart
@@ -7,15 +7,31 @@ class Home extends StatelessWidget {
 
   Future<Map<String, dynamic>> _recuperarPreco() async {
     String url = "https://blockchain.info/ticker";
-    http.Response response = await http.get(url as Uri);
+    http.Response response = await http.get(Uri.parse(url));
     return json.decode(response.body);
   }
 
   @override
   Widget build(BuildContext context) {
-    return  FutureBuilder<Map>(
-        future: _recuperarPreco(),
-        builder: builder
-    ),
+    return FutureBuilder<Map>(
+      future: _recuperarPreco(),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.none:
+            return Text("Conexão None");
+          case ConnectionState.waiting:
+            return CircularProgressIndicator();
+          case ConnectionState.active:
+            return Text("Conexão Active");
+          case ConnectionState.done:
+            if (snapshot.hasError) {
+              return Text("Erro: ${snapshot.error}");
+            } else {
+              // Aqui você pode retornar o widget que deseja baseado no resultado do Future
+              return Text("Valor: ${snapshot.data}");
+            }
+        }
+      },
+    );
   }
 }
